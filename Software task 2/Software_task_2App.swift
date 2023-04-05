c6f4048104ab0922639ba6ac05a105598964db8e
dylanlewis <lewis.dylan.jan@gmail.com>
2023-04-06 09:13:12 +1000

    first xcode

---
+++ Software task 2/Software_task_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Software_task_2App.swift
+//  Software task 2
+//
+//  Created by Dylan Lewis on 6/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct Software_task_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Software task 2UITests/Software_task_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Software_task_2UITestsLaunchTests.swift
+//  Software task 2UITests
+//
+//  Created by Dylan Lewis on 6/4/2023.
+//
+
+import XCTest
+
+final class Software_task_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
