e1df06f70133d3c4fa74ecda4da254d6a66e4a65
MillinsButcher <79286659+MillinsButcher@users.noreply.github.com>
2023-05-20 12:55:40 +1000

    Linked the pages together, made a start on connecting the api and put all the files together
    The files being in seperate folders was the issue with connecting them

---
+++ api.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var apiKey = "644f2b75347d438b8eda2eed10c93404";
+    ?apiKey=644f2b75347d438b8eda2eed10c93404;
+    var url = "https://api.spoonacular.com/recipes/complexSearch";  // replace with the actual API URL
+
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "X-Api-Key": apiKey
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        var recipeContainer = document.getElementById('recipeContainer');
+        data.recipes.forEach(recipe => {
+            var recipeElement = document.createElement('div');
+            recipeElement.innerHTML = `
+                <h2>${recipe.name}</h2>
+                <p>${recipe.description}</p>
+            `;
+            recipeContainer.appendChild(recipeElement);
+        });
+    })
+    .catch(error => console.error('Error:', error));
+});
