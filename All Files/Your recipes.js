feee0f47c30794c74a3926ae7f65a9c8dda31521
dylanlewis <lewis.dylan.jan@gmail.com>
2023-06-22 09:26:25 +1000

    Fixed up the your recipie page so that it has a modal pop up when the view recipie button is pressed

--- All Files/Your recipes.js
+++ All Files/Your recipes.js
@@ -9,6 +9,12 @@ function deleteRecipe(event) {
   saveRecipes();
 }
 
+// Function to view a recipe
+function viewRecipe(event) {
+  const recipeCard = event.target.parentNode;
+  alert(`Recipe: ${recipeCard.querySelector('h3').textContent}\n\nDescription: ${recipeCard.querySelector('p').textContent}`);
+}
+
 // Function to save recipes in local storage
 function saveRecipes() {
   const recipes = Array.from(recentRecipesContainer.children).map(recipeCard => {
@@ -47,8 +53,14 @@ addRecipeForm.addEventListener('submit', function(event) {
   deleteButton.className = 'delete-button';
   recipeCard.appendChild(deleteButton);
 
-  // Add event listener for delete button
+  const viewButton = document.createElement('button');
+  viewButton.textContent = 'View Recipe';
+  viewButton.className = 'view-button';
+  recipeCard.appendChild(viewButton);
+
+  // Add event listeners for delete and view buttons
   deleteButton.addEventListener('click', deleteRecipe);
+  viewButton.addEventListener('click', viewRecipe);
 
   // Add the recipe card to the recent-recipes container
   recentRecipesContainer.appendChild(recipeCard);
@@ -81,31 +93,63 @@ window.addEventListener('load', function() {
     deleteButton.className = 'delete-button';
     recipeCard.appendChild(deleteButton);
 
+    const viewButton = document.createElement('button');
+    viewButton.textContent = 'View Recipe';
+    viewButton.className = 'view-button';
+    recipeCard.appendChild(viewButton);
+
     deleteButton.addEventListener('click', deleteRecipe);
+    viewButton.addEventListener('click', viewRecipe);
 
     recentRecipesContainer.appendChild(recipeCard);
   });
-    function storeRecipe() {
-      event.preventDefault();
-      var recipeName = document.getElementById('recipe-name').value;
-      var recipeDescription = document.getElementById('recipe-description').value;
-    
-      var recipe = {
-        name: recipeName,
-        description: recipeDescription
-      };
-    
-      if(localStorage.getItem('recipes') === null) {
-        localStorage.setItem('recipes', JSON.stringify([recipe]));
-      } else {
-        var existingRecipes = JSON.parse(localStorage.getItem('recipes'));
-        existingRecipes.push(recipe);
-        localStorage.setItem('recipes', JSON.stringify(existingRecipes));
-      }
-    
-      document.getElementById('recipe-name').value = "";
-      document.getElementById('recipe-description').value = "";
-    }
-  
 });
 
+function storeRecipe() {
+  event.preventDefault();
+  var recipeName = document.getElementById('recipe-name').value;
+  var recipeDescription = document.getElementById('recipe-description').value;
+  
+  var recipe = {
+      name: recipeName,
+      description: recipeDescription
+  };
+  
+  if(localStorage.getItem('recipes') === null) {
+      localStorage.setItem('recipes', JSON.stringify([recipe]));
+  } else {
+      var existingRecipes = JSON.parse(localStorage.getItem('recipes'));
+      existingRecipes.push(recipe);
+      localStorage.setItem('recipes', JSON.stringify(existingRecipes));
+  }
+  
+  document.getElementById('recipe-name').value = "";
+  document.getElementById('recipe-description').value = "";
+}
+// Function to view a recipe
+function viewRecipe(event) {
+  const recipeCard = event.target.parentNode;
+  document.getElementById('modal-recipe-name').textContent = recipeCard.querySelector('h3').textContent;
+  document.getElementById('modal-recipe-description').textContent = recipeCard.querySelector('p').textContent;
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // Show the modal
+  modal.style.display = "block";
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
